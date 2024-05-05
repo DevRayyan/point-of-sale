7efc1e53cb598b40978f304bafb69b3755e69b60
Rayyan Ali <113789630+DevRayyan@users.noreply.github.com>
2024-05-05 09:26:47 -0400

    create a login form

--- app.py
+++ app.py
@@ -3,17 +3,25 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
+
+# /common
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('common/home.html')
+
+@app.route('/login')
+def login():
+    return render_template('common/login.html')
 
 @app.route('/pos')
 def pos():
-    return render_template('pos.html')
+    return render_template('common/pos.html')
 
+# /catalog
 @app.route('/catalog/categories')
 def categories():
     return render_template('catalog/categories.html')
 
+# start 
 if __name__ == '__main__':
     app.run(debug=True)
