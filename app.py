5adfd634b86659722215ab4897bf403986e7737d
Rayyan Ali <113789630+DevRayyan@users.noreply.github.com>
2024-05-07 15:45:34 -0400

    add ajax datatable in category list view

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
+from math import ceil
+
 from flask_sqlalchemy import SQLAlchemy
-from flask_paginate import Pagination, get_page_args
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/pos'
 db = SQLAlchemy(app)
-
-# Function to fetch data for the current page
-def get_data_for_page(page_num, per_page):
-    offset = (page_num - 1) * per_page
-    return Category.query.limit(per_page).offset(offset).all()
 
 
 class Category(db.Model):
@@ -35,19 +31,40 @@ def pos():
     return render_template('common/pos.html')
 
 # /catalog
-@app.route('/catalog/categories')
-def categories():
-    page = int(request.args.get('page', 1))
-    per_page = 8
-    data = get_data_for_page(page, per_page)
+@app.route('/catalog/category')
+def category():
+    return render_template('catalog/category.html')
 
-    total_items = Category.query.count()
+@app.route('/get-list')
+def getList():
 
-    total_pages = (total_items + per_page - 1) // per_page
-    
-    return render_template('catalog/categories.html',list=data,page=page, total_pages=total_pages,total_items=total_items)
+    rows = Category.query.all()
+   
+    data = [{'checkbox': ''' 
+                    <div class="flex items-center">
+                        <input id="checkbox-table-search-1" value="1" type="checkbox"
+                            class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                    </div>
+                ''',
+             'id': row.id,
+             'name': row.name,
+             'status': row.status,
+             'created_at': row.created_at.strftime('%d-%m-%Y %H:%M'),
+             'updated_at': row.updated_at.strftime('%d-%m-%Y %H:%M'),
+             'action':'''
+             <button type="button" class="text-white bg-blue-400 hover:bg-blue-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm w-7 h-7 me-1 dark:bg-blue-500 dark:hover:bg-blue-600 focus:outline-none dark:focus:ring-blue-800"><i class="fa-light fa-pen"></i></button>
+             <button type="button" class="text-white bg-red-400 hover:bg-red-500 focus:ring-4 focus:ring-red-300 font-medium rounded-md text-sm w-7 h-7 me-1 dark:bg-red-500 dark:hover:bg-red-600 focus:outline-none dark:focus:ring-red-800"><i class="fa-light fa-trash"></i></button>
 
+             ''',
+             } 
+            for row in rows]
+        
+    response = {'data': data}
+
+    return jsonify(response)
 
 # start 
 if __name__ == '__main__':
     app.run(debug=True)
+
